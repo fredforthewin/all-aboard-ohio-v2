d8329cf1a5cca114c89e6792179a6dd4cf101a4c
fredforthewin <150601495+fredforthewin@users.noreply.github.com>
2023-12-12 21:12:15 -0500

    fixed buttons, top margin, navigation appearance

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,52 @@
-console.log("hello world");
-function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += " responsive";
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
   } else {
-    x.className = "navtoggle";
+    x.style.display = "flex";
+    x.style.flexDirection = "column";
   }
 }
 
-$(document).ready(function () {
-  // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
-      // Store hash
-      var hash = this.hash;
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
-      // Using jQuery's animate() method to add smooth page scroll
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    }
-  });
-});
+// console.log("hello world");
+// function menuToggle() {
+//   var x = document.getElementById("myNavtoggle");
+//   if (x.className === "navtoggle") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "navtoggle";
+//   }
+// }
+
+// $(document).ready(function () {
+//   // Add smooth scrolling to all links
+//   $("a").on("click", function (event) {
+//     if (this.hash !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+
+//       // Store hash
+//       var hash = this.hash;
+
+//       // Using jQuery's animate() method to add smooth page scroll
+//       $("html, body").animate(
+//         {
+//           scrollTop: $(hash).offset().top,
+//         },
+//         800,
+//         function () {
+//           // Add hash (#) to URL when done scrolling (default click behavior)
+//           window.location.hash = hash;
+//         }
+//       );
+//     }
+//   });
+// });
